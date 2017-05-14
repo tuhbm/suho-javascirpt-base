a09e954ff9729786db6808b69e585ae352fad4a1
taekyunKim <tuhbm2@gmail.com>
2017-05-14 11:46:23 +0900

    test

--- 170514/test1.js
+++ 170514/test1.js
@@ -1,11 +1,15 @@
 var math = {
-    sum : function(arr){
-        arr.map(function(arr){
-            return arr
-        })
+    sum : function(arr,aaa){
+        for (var i = 0; i < arguments.length; i++) {
+            var obj = arguments[i];
+            
+        }
     },
     max: function(){
-
+        for (var i = 0; i < arguments.length; i++) {
+            var obj = arguments[i];
+            
+        }
     },
 
 }
